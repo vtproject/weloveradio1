de46f3900bb0f412b2dab81ea9640f1ebaea1baa
ac <ales.cerny@gmail.com>
2021-08-09 06:08:20 +0200

    cover download

--- html_generator.py
+++ html_generator.py
@@ -314,7 +314,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 html_list_tracks = '        <li><span style="color:red">' + arrow +  '</span><a href = "track_detail_' + detail_file_number + '.html">' + chart_list_tracks[item][0] + ' <img src="link.png" width="10" height="10"></a> (' + str(chart_list_tracks[item][1]) + ')</li>\n'
                 html_list_tracks_bold = '        <li><span style="color:red">' + arrow +  '</span><a href = "track_detail_' + detail_file_number + '.html"><b>' + chart_list_tracks[item][0] + '</b> <img src="link.png" width="10" height="10"></a> (' + str(chart_list_tracks[item][1]) + ')</li>\n'
 
-                # html_detail_generator.main(str(chart_list_tracks[item][2]), str(chart_list_tracks[item][3]),detail_file_number,days_back[chart_no],chart_period[chart_no])
+                html_detail_generator.main(str(chart_list_tracks[item][2]), str(chart_list_tracks[item][3]),detail_file_number,days_back[chart_no],chart_period[chart_no])
                 detail_number_2 += 1
                 
             if item == 0:
